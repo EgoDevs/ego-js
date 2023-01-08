fccb4816be68fb7439c1b75102341a188743fdcd
neeboo <ghostcorn@gmail.com>
2023-01-08 23:00:01 +0800

    :fire: remove create

--- apps/create-ego-app/bin/cli.js
+++ apps/create-ego-app/bin/cli.js
@@ -1,10 +1,6 @@
 #!/usr/bin/env node
-var program = require('commander').program;
-program
-  .command('create <app-name>')
-  .description('create a new project')
-  .option('-f, --force', 'overwrite target directory if it exist')
-  .action(function (name, options) {
-    require('./generator')(name, options);
-  });
+var program = require("commander").program;
+program.argument("<app-name>").description("create a new project").option("-f, --force", "overwrite target directory if it exist").action(function(name, options) {
+    require("./generator")(name, options);
+});
 program.parse(process.argv);
--- apps/create-ego-app/src/cli.js
+++ apps/create-ego-app/src/cli.js
@@ -4,8 +4,9 @@ const { program } = require('commander');
 
 // import figlet from 'figlet';
 // import chalk from 'chalk';
+
 program
-  .command('create <app-name>')
+  .argument('<app-name>')
   .description('create a new project')
   .option('-f, --force', 'overwrite target directory if it exist') // 是否强制创建，当文件夹已经存在
   .action((name, options) => {
